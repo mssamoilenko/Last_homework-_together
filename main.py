5698e2debd9df319af4bcd3f0ea860ba606e4002
Alla <allasamoilenko@gmx.de>
2024-10-19 13:30:50 +0300

    add skeleton

---
+++ main.py
@@ -0,0 +1,23 @@
+FILENAME = []
+def load_task():
+    pass
+load_task()
+def save_task():
+    task = []
+    pass
+save_task()
+def add_task():
+    pass
+add_task()
+def edit_task():
+    pass
+edit_task()
+def delete_task():
+    pass
+delete_task()
+def mark_ready_task():
+    pass
+mark_ready_task()
+def main():
+    pass
+main()
