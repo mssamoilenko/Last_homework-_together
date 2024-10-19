90dad892bec04acd58fd4c637f5e1389fe425bb7
Alla <allasamoilenko@gmx.de>
2024-10-19 16:15:39 +0300

    add load, save

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-FILENAME = []
-def load_task():
-    pass
-load_task()
-def save_task():
-    task = []
-    pass
-save_task()
+filename = "tasks.txt"
+filename2 = "saved_tasks.txt"
+
+def load_task(filename):
+    tasks = []
+    with open(filename, 'r', encoding='utf-8') as file:
+        tasks = file.read().splitlines()
+    return tasks
+
+def save_task(filename2, tasks):
+    with open(filename2, 'w', encoding='utf-8') as file:
+        for task in tasks:
+            file.write(task + '\n')
 def add_task():
     pass
 add_task()
